d693876c80bc4f8ff907c20883db75c56dee96f5
boozegar <2654360399@qq.com>
2022-04-10 11:42:36 +0800

    first commit

---
+++ src/main/java/com/boo/SecKillApplication.java
@@ -0,0 +1,16 @@
+package com.boo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 宋
+ */
+@SpringBootApplication
+public class SecKillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecKillApplication.class, args);
+    }
+
+}
